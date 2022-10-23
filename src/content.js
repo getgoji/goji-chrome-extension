0bcc5644a3ad679e883a1c4cd55b5b582d41195a
Kenneth Yang <kjy5@uw.edu>
2022-10-23 03:54:05 -0700

    Resize popup, move JS to popup

--- src/content.js
+++ src/content.js
@@ -1,15 +1,5 @@
 console.log("We see this website: " + document.location.href);
 
-chrome.storage.sync.set({ "preferences": [0, 1, 2] }, () => {
-    console.log("Set preferences to [0,1,2]");
-})
-
-chrome.storage.sync.get(['preferences'], (storage) => {
-    storage.preferences.forEach(preference => {
-        console.log("Preferences are: " + preference);
-    });
-});
-
 const legalWebsites = ["https://www.forever21.com",
     "https://www.adidas.com",
     "https://www.thereformation.com",
---
+++ src/popup.js
@@ -0,0 +1,9 @@
+chrome.storage.sync.set({ "preferences": [0, 1, 2] }, () => {
+    console.log("Set preferences to [0,1,2]");
+})
+
+chrome.storage.sync.get(['preferences'], (storage) => {
+    storage.preferences.forEach(preference => {
+        console.log("Preferences are: " + preference);
+    });
+});
