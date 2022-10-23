8c454075812e31bffdec1be214063f20021aefdf
Bregolian <androodk@gmail.com>
2022-10-23 02:44:24 -0700

    Add image to widget

--- src/content.js
+++ src/content.js
@@ -1,10 +1,10 @@
 console.log("We see this website: " + document.location.href);
 const legalWebsites = ["https://www.forever21.com",
-                       "https://www.adidas.com",
-                       "https://www.thereformation.com",
-                       "https://www.sears.com",
-                       "https://www.zara.com",
-                       "http://example.com"];
+    "https://www.adidas.com",
+    "https://www.thereformation.com",
+    "https://www.sears.com",
+    "https://www.zara.com",
+    "http://example.com"];
 
 legalWebsites.forEach(checkIfClothingBrand);
 
@@ -15,16 +15,22 @@ function checkIfClothingBrand(url) {
     }
 }
 
-function popup(mylink, windowname) { 
+function popup(mylink, windowname) {
     // Get HTML tag
     const html = document.querySelector("html");
 
+    // Create Goji icon image
+    const image = document.createElement("img");
+    image.src = chrome.runtime.getURL("icons/icon-32.png");
+    image.alt = "Goji icon";
+
     // Create overlay
-    const overlay = document.createElement('div');
+    const overlay = document.createElement("div");
     overlay.id = "overlay";
+    overlay.append(image);
 
     // Inject overlay
-    html.insertAdjacentElement("beforeend", overlay);
+    html.append(overlay);
 
 
     // if (! window.focus)return true; 
