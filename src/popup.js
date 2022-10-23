db2c6622c8aacc7058dfe873e0e780a0601954a7
Kenneth Yang <kjy5@uw.edu>
2022-10-23 04:15:39 -0700

    Connect toggles to function

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,40 @@
+// Connect toggle to each category
+const carbon_emissions = document.getElementById("carbon_emissions");
+carbon_emissions.addEventListener("click", () => {
+    togglePreference(0);
+});
+
+const water_usage = document.getElementById("water_usage");
+water_usage.addEventListener("click", () => {
+    togglePreference(1);
+});
+
+const ethical_sourcing = document.getElementById("ethical_sourcing");
+ethical_sourcing.addEventListener("click", () => {
+    togglePreference(2);
+});
+
+const labor_rights = document.getElementById("labor_rights");
+labor_rights.addEventListener("click", () => {
+    togglePreference(3);
+});
+
+const transparency = document.getElementById("transparency");
+transparency.addEventListener("click", () => {
+    togglePreference(4);
+});
+
+const dei = document.getElementById("dei");
+dei.addEventListener("click", () => {
+    togglePreference(5);
+});
+
+// Handle toggle
+function togglePreference(category) {
+    console.log("Toggling preference for " + category);
+}
+
+// Loading settings
 chrome.storage.sync.set({ "preferences": [0, 1, 2] }, () => {
     console.log("Set preferences to [0,1,2]");
 })
