3bbe3306f5e0afd516854dd7d36dd2f93dc811ba
Kenneth Yang <kjy5@uw.edu>
2022-10-23 01:41:00 -0700

    Store and retrieve settings

--- src/content.js
+++ src/content.js
@@ -1,1 +1,11 @@
 console.log("We see this website: " + document.location.href);
+
+chrome.storage.sync.set({ "preferences": [0, 1, 2] }, () => {
+    console.log("Set preferences to [0,1,2]");
+})
+
+chrome.storage.sync.get(['preferences'], (storage) => {
+    storage.preferences.forEach(preference => {
+        console.log("Preferences are: " + preference);
+    });
+});
