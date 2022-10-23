0c476157d63fc8c939feae3413d9a275378d8b54
Kenneth Yang <kjy5@uw.edu>
2022-10-23 04:44:23 -0700

    working?

--- src/popup.js
+++ src/popup.js
@@ -1,54 +1,52 @@
 // User preferences local copy
-let userPreferences = [0, 1, 2];
+let userPreferences;
+const categories = document.querySelectorAll('input[type="checkbox"]');
 
 // Handle on load stuff
 document.addEventListener("DOMContentLoaded", () => {
-    // Load previous settings
+    // Load previous settings (or set new ones)
     chrome.storage.sync.get(['preferences'], (storage) => {
-        userPreferences = storage.preferences;
+        if (storage.preferences == undefined || storage.preferences.length < 3) {
+            userPreferences = [0, 1, 2];
+            chrome.storage.sync.set({ 'preferences': userPreferences });
+        } else {
+            userPreferences = storage.preferences;
+        }
+        // Apply settings
+        updateToggles();
     });
 
     // Add event listeners
-    const carbon_emissions = document.getElementById("carbon_emissions");
-    carbon_emissions.addEventListener("click", () => {
-        togglePreference(0);
-    });
-
-    const water_usage = document.getElementById("water_usage");
-    water_usage.addEventListener("click", () => {
-        togglePreference(1);
-    });
-
-    const ethical_sourcing = document.getElementById("ethical_sourcing");
-    ethical_sourcing.addEventListener("click", () => {
-        togglePreference(2);
-    });
-
-    const labor_rights = document.getElementById("labor_rights");
-    labor_rights.addEventListener("click", () => {
-        togglePreference(3);
-    });
-
-    const transparency = document.getElementById("transparency");
-    transparency.addEventListener("click", () => {
-        togglePreference(4);
-    });
-
-    const dei = document.getElementById("dei");
-    dei.addEventListener("click", () => {
-        togglePreference(5);
-    });
-
-    // Apply settings
-    const categories = [carbon_emissions, water_usage, ethical_sourcing, labor_rights, transparency, dei];
     for (let i = 0; i < categories.length; i++) {
-        categories[i].checked = userPreferences.includes(i);
+        categories[i].addEventListener('click', () => {
+            togglePreference(i);
+        });
     }
 });
 
 
 // Handle toggle
 function togglePreference(category) {
-    console.log("Toggling preference for " + category);
+    // Remove category
+    if (userPreferences.includes(category)) {
+        userPreferences.splice(userPreferences.indexOf(category), 1);
+    } else {
+        // Add category and remove last one
+        userPreferences.pop();
+        userPreferences.unshift(category);
+    }
+
+    // Save settings
+    chrome.storage.sync.set({ 'preferences': userPreferences });
+
+    // Update UI
+    updateToggles();
+}
+
+// Update toggle UI based on user preferences
+function updateToggles() {
+    for (let i = 0; i < categories.length; i++) {
+        categories[i].checked = userPreferences.includes(i);
+    }
 }
 
