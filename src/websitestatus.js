7107e939cbc5b2f131dfa33a1da2b609d5036e6b
Bregolian <androodk@gmail.com>
2022-10-23 07:40:38 -0700

    YES

--- src/websitestatus.js
+++ src/websitestatus.js
@@ -1,20 +1,64 @@
-// Set name
+// Set store name
 const storeName = document.querySelector("#store_name");
+let currentWebsite;
 chrome.storage.local.get(["current_website"], (result) => {
-    storeName.innerHTML = result.current_website;
+    currentWebsite = result.current_website;
+    storeName.innerHTML = currentWebsite;
 });
 
 // Load and sort preferences
 let preferencesSorted;
-chrome.storage.sync.get(["preferences"], (result) => {
-    preferencesSorted = storage.preferences.sort();
+chrome.storage.sync.get(['preferences'], (storage) => {
+    if (storage.preferences === undefined) {
+        chrome.storage.sync.set({ 'preferences': [0, 1, 2] });
+        preferencesSorted = [0, 1, 2];
+    } else {
+        preferencesSorted = storage.preferences.sort();
+    }
 });
 
 // Get category divs
-const category0Div = document.getElementById("#cat0");
-const category1Div = document.getElementById("#cat1");
-const category2Div = document.getElementById("#cat2");
+const categoryPercentiles = document.querySelectorAll("#category_percentile");
+const categoryNames = document.querySelectorAll("#category_name");
+const categoryNamesText = ["Carbon Emissions", "Water Usage", "Ethical Sourcing", "Labor Rights", "Transparency & Policy", "Diversity, Equity, & Inclusion"];
+
+fetch("../percentile.json").then(res => res.json()).then((data) => {
+    // Get data current website index
+    const currentWebsiteIndex = data["columns"].indexOf(currentWebsite);
+
+    // For each favorited data category, print the percentile and category name
+    for (let i = 0; i < 3; i++) {
+        const categoryIndex = preferencesSorted[i];
+        const thisCategory = data["data"][categoryIndex][currentWebsiteIndex];
+        categoryPercentiles[i].innerHTML = thisCategory;
+        categoryNames[i].innerHTML = categoryNamesText[categoryIndex];
+    }
+});
+
+const overallGojiScorePercentile = 85; //TEMP VAL
+printGojiScore(overallGojiScorePercentile, "goji_score1");
+const personalizedGojiScorePercentile = 20; //TEMP VAL
+printGojiScore(personalizedGojiScorePercentile, "goji_score2");
 
 
 
-category0Div.innerHTML = "";
+function printGojiScore(gojiScorePercentile, divName) {
+    const numBerriesPrinted = 0;
+    const overallGojiScore = document.getElementById(divName);
+    while (numBerriesPrinted < 5) {
+        const image = document.createElement("img");
+        image.id = "image";
+        image.alt = "Goji icon";
+        if (gojiScorePercentile > 20) {
+            image.src = chrome.runtime.getURL("icons/goji-icon.png");
+            gojiScorePercentile -= 20;
+        } else if (gojiScorePercentile > 10){
+            image.src = chrome.runtime.getURL("icons/half-goji.png");
+            gojiScorePercentile -= 10;
+        } else {
+            image.src = chrome.runtime.getURL("icons/sad-goji.png");
+        }
+        overallGojiScore.append(image);
+        numBerriesPrinted++;
+    }
+}
