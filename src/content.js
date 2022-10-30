2e0ad71ffbc10439593d8161c59f8dceba1183f4
Jasmine Schoch <jasmineyschoch@gmail.com>
2022-10-30 16:33:51 -0700

    Added all of the sites to dictionary
    Co-authored-by: virginiavw <virginiavw@users.noreply.github.com>
    Co-authored-by: KuhnTycoon <KuhnTycoon@users.noreply.github.com>
    Co-authored-by: Kenneth Yang <kjy5@users.noreply.github.com>

--- src/content.js
+++ src/content.js
@@ -3,14 +3,56 @@ const legalWebsites = {
     "American Eagle Outfitters": "https://www.ae.com",
     "Allbirds": "https://www.allbirds.com",
     "Amazon": "https://www.amazon.com",
-    "Forever 21": "https://www.forever21.com",
     "Adidas Group": "https://www.adidas.com",
+    "Boohoo": "https://us.boohoo.com",
+    "Boyish Jeans": "https://www.boyish.com",
+    "Burberry": "https://us.burberry.com",
+    "Brother Vellies": "https://brothervellies.com",
+    "C&A": "https://www.c-and-a.com",
+    "Christy Dawn": "https://christydawn.com/",
+    "Eileen Fisher": "https://www.eileenfisher.com/",
+    "Everlane": "https://www.everlane.com/",
+    "Fashion Nova": "https://www.fashionnova.com/",
+    "Filippa K": "https://www.filippa-k.com/",
+    "Forever 21": "https://www.forever21.com",
+    "Gap Inc.": "https://www.gap.com/",
+    "Girlfriend Collective": "https://girlfriend.com/",
     "Lululemon": "https://shop.lululemon.com",
     "Reformation": "https://www.thereformation.com",
     "Gucci": "https://www.gucci.com",
-    "Kohl's": "https://www.kohls.com/"
+    "Kohl's": "https://www.kohls.com/",
+    "M&S": "https://www.marksandspencer.com/",
+    "Missguided": "https://www.missguided.co.uk/",
+    "Mothercare": "https://www.mothercare.com.sg/",
+    "MUD Jeans": "https://mudjeans.eu/",
+    "Next": "https://www.next.co.uk/",
+    "Nike": "https://www.nike.com/",
+    "Mara Hoffman": "https://marahoffman.com/",
+    "Nisolo": "https://nisolo.com/",
+    "Patagonia": "https://www.patagonia.com",
+    "Primark": "https://www.primark.com",
+    "Ralph Lauren": "https://www.ralphlauren.com/",
+    "Ross": "https://www.rossstores.com/",
+    "SHEIN": "https://us.shein.com",
+    "Stella McCartney": "https://www.stellamccartney.com",
+    "Target": "https://www.target.com",
+    "The Children's Place": "https://www.childrensplace.com",
+    "H&M Group": "https://www2.hm.com/en_us",
+    "Inditex": "https://www.inditex.com",
+    "J. Crew Inc.": "https://www.jcrew.com",
+    "JCPenney": "https://www.jcpenney.com",
+    "Levi Strauss": "https://www.levi.com",
+    "The Edinburgh Woollen Mill": "https://www.ewm.co.uk",
+    "Under Armour": "https://www.underarmour.com/en-us",
+    "URBN": "https://www.urbanoutfitters.com",
+    "VEJA": "https://www.veja-store.com",
+    "Victoria's Secret & Co": "https://www.victoriassecret.com",
+    "Walmart": "https://www.walmart.com",
+    "Zalando": "https://www.zalando",
+
 };
-// "URBN","Besteller","Boohoo","Boyish Jeans","Brother Vellies","Burberry","C&A","Christy Dawn","Eileen Fisher","Everlane","Forever 21","Fashion Nova","Filippa K","Gap Inc.","Girlfriend Collective","Global Brands Group","Gucci","H&M Group","Inditex","J. Crew Inc.","JCPenney","Kohl's","Levi Strauss","Mara Hoffman","M&S","Missguided","Mothercare","MUD Jeans","Next","Nike","Nisolo","Patagonia","Primark","PVH","Ralph Lauren","Reformation","Ross","SHEIN","Stella McCartney","Target","The Children's Place","The Edinburgh Woollen Mill","TJX","Under Armour","Fast Retailing (Uniqlo)","VEJA","VF Corp","Victoria's Secret & Co","Walmart","Zalando"
+
+// Parent Companies: "Besteller", "VF Corp", "TJX", "Fast Retailing (Uniqlo)", "PVH", "Gap Inc.", "Global Brands Group",
 
 Object.entries(legalWebsites).forEach(([name, url]) => {
     if (document.location.href.includes(url)) {
@@ -21,7 +63,7 @@ Object.entries(legalWebsites).forEach(([name, url]) => {
 
 function popup() {
     // Get HTML tag
-    const html = document.querySelector("html");
+    const html = document.querySelector("html"); 
 
     // Create Goji icon image
     const image = document.createElement("img");
