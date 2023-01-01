082b8ff698285bb79daa3ff4950f26fd9ce43c85
microBob <microBob@users.noreply.github.com>
2023-01-01 03:05:34 +0000

    Reformat and Lint

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Goji Chrome Extension
+
 Seeding Sustainability. A Chrome extensions that helps you see how sustainable your favorite fashion brands are!
 
 Learn more about this amazing platform here at [getgoji.tech](http://getgoji.tech)
@@ -8,14 +9,15 @@ Learn more about this amazing platform here at [getgoji.tech](http://getgoji.tec
 You can install the extension from the [Chrome Web Store](https://chrome.google.com/webstore/detail/goji/pjeeheedkpjojpgkpmbopfcopniinfdn)
 
 ## Manual Installation (and for development)
+
 1. Clone or download this repository and extract the contents
 2. In your Chrome-based browser (Google Chrome, Microsoft Edge, etc.), navigate to `chrome://extensions`
 3. Enable Developer Mode by clicking the toggle switch next to Developer mode.
 4. Click the Load unpacked button and select the extension directory.
 5. Navigate to your favorite brand's website and see how they rank on Goji!
 
-
 # Credits
+
 Extension code: Andrew Kuhn & Kenneth Yang
 
 [Data extraction and processing](https://github.com/jasmine-schoch/goji-data-analysis): Jasmine Schoch & Virginia Wang
--- src/content.js
+++ src/content.js
@@ -1,103 +1,105 @@
 const legalWebsites = {
-    "A&F": "https://www.abercrombie.com",
-    "American Eagle Outfitters": "https://www.ae.com",
-    "Allbirds": "https://www.allbirds.com",
-    "Amazon": "https://www.amazon.com",
-    "Adidas Group": "https://www.adidas.com",
-    "Boohoo": "https://us.boohoo.com",
-    "Boyish Jeans": "https://www.boyish.com",
-    "Burberry": "https://us.burberry.com",
-    "Brother Vellies": "https://brothervellies.com",
-    "C&A": "https://www.c-and-a.com",
-    "Christy Dawn": "https://christydawn.com/",
-    "Eileen Fisher": "https://www.eileenfisher.com/",
-    "Everlane": "https://www.everlane.com/",
-    "Fashion Nova": "https://www.fashionnova.com/",
-    "Filippa K": "https://www.filippa-k.com/",
-    "Forever 21": "https://www.forever21.com",
-    "Gap Inc.": "https://www.gap.com/",
-    "Girlfriend Collective": "https://girlfriend.com/",
-    "Lululemon": "https://shop.lululemon.com",
-    "Reformation": "https://www.thereformation.com",
-    "Gucci": "https://www.gucci.com",
-    "Kohl's": "https://www.kohls.com/",
-    "M&S": "https://www.marksandspencer.com/",
-    "Missguided": "https://www.missguided.co.uk/",
-    "Mothercare": "https://www.mothercare.com.sg/",
-    "MUD Jeans": "https://mudjeans.eu/",
-    "Next": "https://www.next.co.uk/",
-    "Nike": "https://www.nike.com/",
-    "Mara Hoffman": "https://marahoffman.com/",
-    "Nisolo": "https://nisolo.com/",
-    "Patagonia": "https://www.patagonia.com",
-    "Primark": "https://www.primark.com",
-    "Ralph Lauren": "https://www.ralphlauren.com/",
-    "Ross": "https://www.rossstores.com/",
-    "SHEIN": "https://us.shein.com",
-    "Stella McCartney": "https://www.stellamccartney.com",
-    "Target": "https://www.target.com",
-    "The Children's Place": "https://www.childrensplace.com",
-    "H&M Group": "https://www2.hm.com/en_us",
-    "Inditex": "https://www.inditex.com",
-    "J. Crew Inc.": "https://www.jcrew.com",
-    "JCPenney": "https://www.jcpenney.com",
-    "Levi Strauss": "https://www.levi.com",
-    "The Edinburgh Woollen Mill": "https://www.ewm.co.uk",
-    "Under Armour": "https://www.underarmour.com/en-us",
-    "URBN": "https://www.urbanoutfitters.com",
-    "VEJA": "https://www.veja-store.com",
-    "Victoria's Secret & Co": "https://www.victoriassecret.com",
-    "Walmart": "https://www.walmart.com",
-    "Zalando": "https://www.zalando",
-
+  "A&F": "https://www.abercrombie.com",
+  "American Eagle Outfitters": "https://www.ae.com",
+  Allbirds: "https://www.allbirds.com",
+  Amazon: "https://www.amazon.com",
+  "Adidas Group": "https://www.adidas.com",
+  Boohoo: "https://us.boohoo.com",
+  "Boyish Jeans": "https://www.boyish.com",
+  Burberry: "https://us.burberry.com",
+  "Brother Vellies": "https://brothervellies.com",
+  "C&A": "https://www.c-and-a.com",
+  "Christy Dawn": "https://christydawn.com/",
+  "Eileen Fisher": "https://www.eileenfisher.com/",
+  Everlane: "https://www.everlane.com/",
+  "Fashion Nova": "https://www.fashionnova.com/",
+  "Filippa K": "https://www.filippa-k.com/",
+  "Forever 21": "https://www.forever21.com",
+  "Gap Inc.": "https://www.gap.com/",
+  "Girlfriend Collective": "https://girlfriend.com/",
+  Lululemon: "https://shop.lululemon.com",
+  Reformation: "https://www.thereformation.com",
+  Gucci: "https://www.gucci.com",
+  "Kohl's": "https://www.kohls.com/",
+  "M&S": "https://www.marksandspencer.com/",
+  Missguided: "https://www.missguided.co.uk/",
+  Mothercare: "https://www.mothercare.com.sg/",
+  "MUD Jeans": "https://mudjeans.eu/",
+  Next: "https://www.next.co.uk/",
+  Nike: "https://www.nike.com/",
+  "Mara Hoffman": "https://marahoffman.com/",
+  Nisolo: "https://nisolo.com/",
+  Patagonia: "https://www.patagonia.com",
+  Primark: "https://www.primark.com",
+  "Ralph Lauren": "https://www.ralphlauren.com/",
+  Ross: "https://www.rossstores.com/",
+  SHEIN: "https://us.shein.com",
+  "Stella McCartney": "https://www.stellamccartney.com",
+  Target: "https://www.target.com",
+  "The Children's Place": "https://www.childrensplace.com",
+  "H&M Group": "https://www2.hm.com/en_us",
+  Inditex: "https://www.inditex.com",
+  "J. Crew Inc.": "https://www.jcrew.com",
+  JCPenney: "https://www.jcpenney.com",
+  "Levi Strauss": "https://www.levi.com",
+  "The Edinburgh Woollen Mill": "https://www.ewm.co.uk",
+  "Under Armour": "https://www.underarmour.com/en-us",
+  URBN: "https://www.urbanoutfitters.com",
+  VEJA: "https://www.veja-store.com",
+  "Victoria's Secret & Co": "https://www.victoriassecret.com",
+  Walmart: "https://www.walmart.com",
+  Zalando: "https://www.zalando",
 };
 
 // Parent Companies: "Besteller", "VF Corp", "TJX", "Fast Retailing (Uniqlo)", "PVH", "Gap Inc.", "Global Brands Group",
 
 Object.entries(legalWebsites).forEach(([name, url]) => {
-    if (document.location.href.includes(url)) {
-        chrome.storage.local.set({ 'current_website': name});
-        popup();
-    }
+  if (document.location.href.includes(url)) {
+    chrome.storage.local.set({ current_website: name });
+    popup();
+  }
 });
 
 function popup() {
-    // Get HTML tag
-    const html = document.querySelector("html"); 
+  // Get HTML tag
+  const html = document.querySelector("html");
 
-    // Create Goji icon image
-    const image = document.createElement("img");
-    image.id = "image";
-    image.src = chrome.runtime.getURL("icons/goji-icon.png");
-    image.alt = "Goji icon";
+  // Create Goji icon image
+  const image = document.createElement("img");
+  image.id = "image";
+  image.src = chrome.runtime.getURL("icons/goji-icon.png");
+  image.alt = "Goji icon";
 
-    // Create overlay
-    const overlay = document.createElement("div");
-    overlay.id = "overlay";
-    overlay.append(image);
+  // Create overlay
+  const overlay = document.createElement("div");
+  overlay.id = "overlay";
+  overlay.append(image);
 
-    // Inject overlay
-    html.append(overlay);
+  // Inject overlay
+  html.append(overlay);
 
-    const button = document.getElementById("overlay");
+  const button = document.getElementById("overlay");
 
-    button.addEventListener('click', function handleClick() {
-        console.log('element clicked');
-        popup(chrome.runtime.getURL("components/websitestatus.html"));
-    });
+  button.addEventListener("click", function handleClick() {
+    console.log("element clicked");
+    popup(chrome.runtime.getURL("components/websitestatus.html"));
+  });
 
-    function popup(mylink) {
-        if (!window.focus) {
-            return true;
-        }
-        var href;
-        if (typeof (mylink) == 'string') {
-            href = mylink;
-        } else {
-            href = mylink.href;
-        }
-        window.open(href, "Goji Store Score", 'width=400,height=600,scrollbars=yes,location=0');
-        return false;
+  function popup(mylink) {
+    if (!window.focus) {
+      return true;
     }
+    var href;
+    if (typeof mylink == "string") {
+      href = mylink;
+    } else {
+      href = mylink.href;
+    }
+    window.open(
+      href,
+      "Goji Store Score",
+      "width=400,height=600,scrollbars=yes,location=0"
+    );
+    return false;
+  }
 }
-
