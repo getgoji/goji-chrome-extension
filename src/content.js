57648d7900662c8f514c1fd710696a720fd2a8f1
Microbob µ <microbob@is88.com>
2023-01-01 18:36:46 -0800

    Fixed content js

--- src/content.js
+++ src/content.js
@@ -1,3 +1,4 @@
+// Map company name -> website URL
 const legalWebsites = {
   "A&F": "https://www.abercrombie.com",
   "American Eagle Outfitters": "https://www.ae.com",
@@ -56,50 +57,59 @@ const legalWebsites = {
 Object.entries(legalWebsites).forEach(([name, url]) => {
   if (document.location.href.includes(url)) {
     chrome.storage.local.set({ current_website: name });
-    popup();
+    inject();
   }
 });
 
-function popup() {
+/**
+ * Inject Goji tab into website
+ */
+function inject() {
   // Get HTML tag
   const html = document.querySelector("html");
 
   // Create Goji icon image
   const image = document.createElement("img");
-  image.id = "image";
   image.src = chrome.runtime.getURL("icons/goji-icon.png");
   image.alt = "Goji icon";
 
-  // Create overlay
-  const overlay = document.createElement("div");
-  overlay.id = "overlay";
-  overlay.append(image);
+  // Create tab
+  const gojiTabElement = document.createElement("div");
+  gojiTabElement.id = "goji-tab";
+  gojiTabElement.append(image);
 
-  // Inject overlay
-  html.append(overlay);
+  // Inject tab
+  html.append(gojiTabElement);
 
-  const button = document.getElementById("overlay");
-
-  button.addEventListener("click", function handleClick() {
-    console.log("element clicked");
-    popup(chrome.runtime.getURL("components/websitestatus.html"));
+  // Add click behavior
+  gojiTabElement.addEventListener("click", function handleClick() {
+    present(chrome.runtime.getURL("components/websitestatus.html"));
   });
 
-  function popup(mylink) {
+  /**
+   * Present the Goji website status popup window
+   * @param {string} mylink resolved URL to popup HTML document
+   * @returns {void} returns early if the window is not in focus
+   */
+  function present(mylink) {
+    // Exit early if the window is not in focus
     if (!window.focus) {
-      return true;
+      return;
     }
+
+    // Extract the HTML document url
     var href;
     if (typeof mylink == "string") {
       href = mylink;
     } else {
       href = mylink.href;
     }
+
+    // Present popup
     window.open(
       href,
       "Goji Store Score",
       "width=400,height=600,scrollbars=yes,location=0"
     );
-    return false;
   }
 }
