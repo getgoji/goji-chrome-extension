c2878f2d4eb5aa3343223ac9bf46918c07e19428
Microbob µ <microbob@is88.com>
2023-03-16 01:16:06 -0700

    Populate brand data

--- src/content.js
+++ src/content.js
@@ -18,13 +18,27 @@ fetch(chrome.runtime.getURL("components/brand.html"))
   .then(html => {
     document.body.insertAdjacentHTML("beforeend", html);
 
-    // Load data
+    // Populate brand info card
     getData().then((data) => {
+      // Get data
       let brandData = data[0];
       let preferencesSorted = data[1];
 
-      console.log(brandData);
-      console.log(preferencesSorted);
+      // Apply brand name
+      document.getElementById("brand-name").innerHTML = brandData[DataCol.NAME];
+
+      // Get HTML elements
+      const categoryPercentiles = document.querySelectorAll("#category-percentile");
+      const categoryNames = document.querySelectorAll("#category-name");
+
+      // Populate personalized category percentiles
+      let personalizedPercentileTotal = 0;
+      preferencesSorted.forEach((preference, index) => {
+        const thisCategory = brandData[preference + 1];
+        personalizedPercentileTotal += thisCategory;
+        categoryPercentiles[index].innerHTML = thisCategory;
+        categoryNames[index].innerHTML = CATEGORY_NAMES[preference];
+      });
     })
   })
 
