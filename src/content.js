c3398d931c9488b106d7fec7494884e1ce0268fc
microBob <microBob@users.noreply.github.com>
2023-03-19 04:00:49 +0000

    Reformat and Lint

--- src/content.js
+++ src/content.js
@@ -1,37 +1,41 @@
 // Create host and inject into page
-const host = document.createElement('div');
-host.setAttribute('id', 'goji-card-host');
-document.body.insertAdjacentElement('beforeend', host);
+const host = document.createElement("div");
+host.setAttribute("id", "goji-card-host");
+document.body.insertAdjacentElement("beforeend", host);
 
 // Create shadow DOM
-const shadow = host.attachShadow({ mode: 'open' });
+const shadow = host.attachShadow({ mode: "open" });
 
 // Inject card
 fetch(chrome.runtime.getURL("components/card.html"))
-    .then(response => response.text())
-    .then(html => {
-        // Add HTML
-        shadow.innerHTML = html;
+  .then((response) => response.text())
+  .then((html) => {
+    // Add HTML
+    shadow.innerHTML = html;
 
-        // Add CSS
-        const style = document.createElement('link');
-        style.setAttribute('rel', 'stylesheet');
-        style.setAttribute('href', chrome.runtime.getURL("styles/card.css"));
-        shadow.appendChild(style);
+    // Add CSS
+    const style = document.createElement("link");
+    style.setAttribute("rel", "stylesheet");
+    style.setAttribute("href", chrome.runtime.getURL("styles/card.css"));
+    shadow.appendChild(style);
 
-        // Setup Tab
-        setupTab();
+    // Setup Tab
+    setupTab();
 
-        // Set open function
-        shadow.getElementById("goji-tab").addEventListener("click", () => {
-            // Open tab
-            host.classList.toggle("goji-card-host--open");
+    // Set open function
+    shadow.getElementById("goji-tab").addEventListener("click", () => {
+      // Open tab
+      host.classList.toggle("goji-card-host--open");
 
-            // Swap tab icon
-            shadow.getElementById("goji-tab--icon").classList.toggle("goji-tab--icon--open");
-            shadow.getElementById("goji-tab--cross").classList.toggle("goji-tab--cross--open");
-        });
+      // Swap tab icon
+      shadow
+        .getElementById("goji-tab--icon")
+        .classList.toggle("goji-tab--icon--open");
+      shadow
+        .getElementById("goji-tab--cross")
+        .classList.toggle("goji-tab--cross--open");
+    });
 
-        // Setup brand info
-        setupBrand();
-    })
+    // Setup brand info
+    setupBrand();
+  });
