f4ed25986f55729603aae2f20d0054d28c73787b
Microbob µ <microbob@is88.com>
2023-06-01 01:10:21 -0700

    Add linear progress bar and listed data

--- components/score.tsx
+++ components/score.tsx
@@ -1,17 +1,33 @@
-import type { BrandData } from "./data"
+import LinearProgress from "@mui/material/LinearProgress"
+
+import type { BrandData, Category } from "./data"
+
+/**
+ * Extract category values and return them as components
+ * @param categoryValues Category values
+ * @returns Array of category value components
+ */
+const extractCategoryValues = (categoryValues: Map<Category, number>) => {
+  const output = []
+  categoryValues.forEach((value, category) => {
+    output.push(
+      <>
+        <p>
+          {category} : {value}
+        </p>
+        <LinearProgress variant="determinate" value={value} />
+      </>
+    )
+  })
+  return output
+}
 
 export const Score = (props: { data: BrandData }): JSX.Element => {
   return (
     <>
       <h1>Goji Score</h1>
       <h2>{props.data.name}</h2>
-      {props.data.categoryValues.forEach((value, category, _) => {
-        return (
-          <p>
-            {category}: {value}
-          </p>
-        )
-      })}
+      {extractCategoryValues(props.data.categoryValues)}
     </>
   )
 }
