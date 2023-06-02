505c178de0ef4e713e9fb2612a221186024fed6f
Microbob µ <microbob@is88.com>
2023-06-01 21:15:15 -0700

    Grid based organization, fixed size

--- components/score.tsx
+++ components/score.tsx
@@ -1,6 +1,8 @@
 import LinearProgress from "@mui/material/LinearProgress"
+import Grid from "@mui/material/Unstable_Grid2"
 
 import type { BrandData, Category } from "./data"
+import { Stack } from "@mui/material"
 
 /**
  * Extract category values and return them as components
@@ -11,12 +13,14 @@ const extractCategoryValues = (categoryValues: Map<Category, number>) => {
   const output = []
   categoryValues.forEach((value, category) => {
     output.push(
-      <>
-        <p>
-          {category}: {value}
-        </p>
-        <LinearProgress variant="determinate" value={value} />
-      </>
+      <Grid container spacing={0}>
+        <Grid xs={"auto"}>{category}</Grid>
+        <Grid xs></Grid>
+        <Grid xs={"auto"}>{value}%tile</Grid>
+        <Grid xs={12}>
+          <LinearProgress variant="determinate" value={value} />
+        </Grid>
+      </Grid>
     )
   })
   return output
@@ -27,7 +31,9 @@ export const Score = (props: { data: BrandData }): JSX.Element => {
     <>
       <h1>Goji Score</h1>
       <h2>{props.data.name}</h2>
-      {extractCategoryValues(props.data.categoryValues)}
+      <Stack spacing={2}>
+        {extractCategoryValues(props.data.categoryValues)}
+      </Stack>
     </>
   )
 }
